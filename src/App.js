ab7ade51f94e5503e3208bee655eb9f96d5486c1
hane.hysa <hanehysa@2rmlab.net>
2024-01-20 12:51:02 +0100

    --added: tailwind config properly with react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './assets/logo.png';
-
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Layout>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </Layout>
+    </>
   );
 }
 
